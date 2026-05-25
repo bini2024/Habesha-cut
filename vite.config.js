c79f2d3148a13c78805b319c70c91922b5763eb9
bini2024 <binibrk1@gmail.com>
2026-05-25 17:33:42 -0400

    Matched base exactly to GitHub Pages URL

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/habesha-cuts/', // <-- ALL lowercase, with an "s"!
+  base: '/Habesha-cut/',
 })
